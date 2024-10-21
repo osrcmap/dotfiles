937d84b51ebd0585eac189e184465f2bdb9e0c66
OscarMacchiavello <mapeoscar123@gmail.com>
2024-10-20 20:53:51 -0500

    first commit

---
+++ nvim/lua/plugins-setup.lua
@@ -0,0 +1,59 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		opts = {
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "css", "go", "cpp", "python", "java", "sql", "bash" },
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		},
+	},
+	{
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+	},
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        config = true
+        -- use opts = {} for passing setup options
+        -- this is equivalent to setup({}) function
+    }
+})
+
+require("mason").setup({
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗"
+		}
+	}
+})
+
+require("mason-lspconfig").setup{
+    ensure_installed = {"gopls", "clangd", "pyright", "lua_ls"},
+}
+
+require("lspconfig").gopls.setup{}
+require("lspconfig").clangd.setup{}
+require("lspconfig").pyright.setup{}
+require("lspconfig").lua_ls.setup{}
---
+++ nvim/lua/settings.lua
@@ -0,0 +1,10 @@
+vim.opt.cursorline = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.termguicolors = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
