278feb639d6b9ff60d135d4f226dbec01b2cf910
osrcmap <mapeoscar123@gmail.com>
2025-10-07 19:42:53 -0500

    update to vim.lsp.config API

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,238 @@
-vim.g.mapleader = " " -- important to set this before loading telescpe
+vim.g.mapleader = " "
 
-require("settings")
-require("plugins-setup")
-require("keymaps")
+-- General opts 
+vim.opt.cursorline = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.termguicolors = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.scrolloff = 10
+
+-- Plugin setup (lazy)
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "css", "go", "cpp", "python", "java", "sql", "bash", "typescript", "dockerfile", "yaml" },
+      highlight = { enable = true },
+      indent = { enable = true },
+    },
+  },
+
+  { "williamboman/mason.nvim" },
+  { "williamboman/mason-lspconfig.nvim" },
+  { "neovim/nvim-lspconfig" },
+
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = true
+  },
+
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/cmp-vsnip",
+      "hrsh7th/vim-vsnip",
+    }
+  },
+
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = {
+      variant = "auto",
+      dark_variant = "main",
+      enable = { terminal = true, legacy_highlights = true, migrations = true },
+      styles = { bold = true, italic = true, transparency = true },
+    },
+  },
+
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" }
+  },
+
+  { "windwp/nvim-ts-autotag" },
+  { "mbbill/undotree" },
+})
+
+-- Theme
+vim.cmd("colorscheme rose-pine-main")
+
+-- Mason setup
+require("mason").setup({
+  ui = {
+    icons = {
+      package_installed = "✓",
+      package_pending = "➜",
+      package_uninstalled = "✗"
+    }
+  }
+})
+
+require("mason-lspconfig").setup({
+  ensure_installed = {
+    "gopls", "clangd", "pyright", "lua_ls", "jdtls", "ts_ls", "cssls", "html", "sqlls",
+    "docker_compose_language_service", "dockerls", "nginx_language_server",
+    "tailwindcss", "yamlls"
+  },
+})
+
+-- nvim-cmp setup
+local cmp = require("cmp")
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "vsnip" },
+  }, {
+    { name = "buffer" },
+  }),
+})
+
+cmp.setup.cmdline({ "/", "?" }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = { { name = "buffer" } }
+})
+
+cmp.setup.cmdline(":", {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
+  matching = { disallow_symbol_nonprefix_matching = false },
+})
+
+-- LSP setup
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+local on_attach = function(_, bufnr)
+  local opts = { noremap = true, silent = true, buffer = bufnr }
+  local map = vim.keymap.set
+
+  map("n", "gd", vim.lsp.buf.definition, opts)
+  map("n", "gD", vim.lsp.buf.declaration, opts)
+  map("n", "K", vim.lsp.buf.hover, opts)
+  map("n", "gi", vim.lsp.buf.implementation, opts)
+  map("n", "gr", vim.lsp.buf.references, opts)
+  map("n", "<leader>rn", vim.lsp.buf.rename, opts)
+  map("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+  map("n", "<leader>f", function() vim.lsp.buf.format { async = true } end, opts)
+  map("n", "<leader>e", vim.diagnostic.open_float, opts)
+  map("n", "[d", vim.diagnostic.goto_prev, opts)
+  map("n", "]d", vim.diagnostic.goto_next, opts)
+  map("n", "<leader>q", vim.diagnostic.setloclist, opts)
+end
+
+-- New API for LSP config
+-- LSP setup (per filetype)
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+local on_attach = on_attach
+local lsp = vim.lsp
+
+local servers = { "gopls", "clangd", "pyright", "lua_ls", "jdtls", "ts_ls", "cssls", "html", "sqlls", "docker_compose_language_service", "dockerls", "nginx_language_server", "tailwindcss", "yamlls" }
+
+for _, server in ipairs(servers) do
+  local config = vim.lsp.config[server]
+  if config then
+    -- Register autocmd so LSP starts only when a matching filetype is opened
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = config.filetypes or "*", -- only attach on relevant filetypes
+      callback = function(args)
+
+        local opts = vim.tbl_deep_extend("force", config, {
+          capabilities = capabilities,
+          on_attach = on_attach,
+        })
+
+        -- Resolve root_dir correctly
+        if opts.root_dir and type(opts.root_dir) == "function" then
+          opts.root_dir = opts.root_dir(args.buf)
+        end
+
+        -- Start server for buffer
+        vim.lsp.start(opts)
+      end,
+    })
+  end
+end
+
+-- Lualine
+require("lualine").setup({
+  options = {
+    icons_enabled = true,
+    theme = "horizon",
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
+  },
+  sections = {
+    lualine_a = { "mode" },
+    lualine_b = { "branch", "diff", "diagnostics" },
+    lualine_c = { "filename" },
+    lualine_x = { "encoding", "fileformat", "filetype" },
+    lualine_y = { "progress" },
+    lualine_z = { "location" },
+  },
+})
+
+-- nvim-ts-autotag
+require("nvim-ts-autotag").setup({
+  opts = {
+    enable_close = true,
+    enable_rename = true,
+    enable_close_on_slash = false,
+  },
+  per_filetype = { html = { enable_close = true } },
+})
+
+-- Telescope keymaps
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
--- nvim/lua/keymaps.lua
+++
@@ -1,9 +0,0 @@
--- some keymaps are defined in the plugins-setup.lua
-
--- vim.g.mapleader = " " -- set in init.lua 
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
